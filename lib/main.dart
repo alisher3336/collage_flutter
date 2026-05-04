23dc5341900e84170345373fa04822ca03531e2e
alisher3336 <erzhanovalisher4@gmail.com>
2026-05-04 08:07:04 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,60 +1,42 @@
-import 'package:bridle/game.dart';
-import 'package:flutter/material.dart';
-
-void main() {
-  runApp(const MainApp());
-}
-
-class Title extends StatelessWidget {
-  const Title(this.letter, this.hitType, {super.key});
-  final String letter;
-  final HitType hitType;
+class GuessInput extends StatelessWidget {
+  final Function(String) onSubmitGuess;
+  final TextEditingController _textEditingController = TextEditingController();
+  final FocusNode _focusNode = FocusNode();
+  GuessInput({super.key, required this.onSubmitGuess});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 60,
-      height: 60,
-      decoration: BoxDecoration(
-        border: Border.all(color: Colors.grey.shade300),
-        color: switch (hitType) {
-          HitType.hit => Colors.green,
-          HitType.miss => Colors.grey,
-          HitType.partial => Colors.yellow,
-          _ => Colors.white,
-        },
-      ),
-      child: Center(child: Text(letter.toUpperCase())),
-    );
-  }
-}
-
-class GamePage extends StatelessWidget {
-  GamePage({super.key});
-
-  final Game _game = Game();
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(8.0),
-      child: Column(
-        spacing: 5.0,
-        children: [
-          for (final guess in _game.guesses)
-            Row(
-              spacing: 5.0,
-              children: [
-                for (final letter in guess) Title(letter.char, letter.type),
-              ],
+    return Row(
+      children: [
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(8.0),
+            child: TextField(
+              maxLength: 5, 
+              decoration: InputDecoration(labelText: "Enter your guess", 
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(8.0))
+                  )
+                ),
+              controller: _textEditingController,
+              onSubmitted: (input) {
+                _textEditingController.clear();
+                _focusNode.requestFocus();
+              },
+              focusNode: _focusNode,
             ),
-          GuessInput(
-            onSubmitGuess: (text) {
-              print("тест");
-            },
           ),
-        ],
-      ),
+        ),
+        IconButton(
+          padding: EdgeInsets.all(8.0),
+          icon: Icon(Icons.arrow_circle_up),
+          onPressed: () {
+            onSubmitGuess(_textEditingController.text.trim());
+            _textEditingController.clear();
+            _focusNode.requestFocus();
+          },
+        ),
+      ],
     );
   }
 }
@@ -67,41 +49,8 @@ class MainApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: Center(child: GamePage()),
-        appBar: AppBar(
-          title: Align(alignment: Alignment.centerLeft, child: Text("Bridle")),
-        ),
+        appBar: AppBar(title: Align(alignment: Alignment.centerLeft, child: Text("Bridle"))),
       ),
-    );
-  }
-}
-
-class GuessInput extends StatelessWidget {
-  final Function(String) onSubmitGuess;
-  final TextEditingController _textEditingController = TextEditingController();
-  GuessInput({super.key, required this.onSubmitGuess});
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Expanded(
-          child: Padding(
-            padding: EdgeInsets.all(8.0),
-            child: TextField(
-              maxLength: 5,
-              decoration: InputDecoration(
-                labelText: "Enter your guess",
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(8.0)),
-                ),
-              ),
-              onSubmitted: (input) {
-                print(_textEditingController.text);
-              },
-            ),
-          ),
-        ),
-      ],
     );
   }
 }
